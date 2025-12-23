d5490c82f4c429822b24fbc94a5d795e615c2481
EStoreB <user7@educateai.in>
2025-12-23 14:17:37 +0530

    add App.jsx

---
+++ src/src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Inventory Management UI</h1>
+      <p>GitHub Pages + React + Vite working ✅</p>
+    </div>
+  );
+}
