da913f065f7bfe950138587aa9eaa9f1f67e413a
EStoreB <user7@educateai.in>
2025-12-24 10:18:01 +0530

    Add Sidebar component with menu items

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,15 @@
+function Sidebar() {
+  return (
+    <aside className="sidebar">
+      <h3>Menu</h3>
+      <ul>
+        <li>Dashboard</li>
+        <li>Inventory</li>
+        <li>Categories</li>
+        <li>Suppliers</li>
+      </ul>
+    </aside>
+  );
+}
+
+export default Sidebar;
